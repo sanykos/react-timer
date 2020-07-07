00e83fa9f1f3d089c5fd9596aaac684ac8396ffa
sanykos <sanykos446@gmail.com>
2020-07-08 00:04:52 +0300

    add timewrapper

--- src/components/TimerWrapper.js
+++ src/components/TimerWrapper.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react'
+import Button from './Button'
+import Timer from './Timer'
 
 export class TimerWrapper extends Component {
 
     constructor(props) {
         super(props)
-        console.log('TimerWrapper')
+        this.state = {
+            timerLeft: null,
+            timer: null,
+        }
     }
 
-    startTimer(timeLeft) {
+    startTimer = timerLeft => {
+        clearInterval(this.state.timer)
+        let timer = setInterval(() => {
+            let timerLeft = this.state.timerLeft-1
+            if(timerLeft == 0) clearInterval(timer)
+        }, 1000)
 
+        return this.setState({
+            timerLeft: timerLeft,
+            timer: timer
+        })
     }
 
 
 
     render() {
         return (
-            <div>
-                
+            <div className="container">
+                <h2>Timer</h2>
+                <div className="btn-group">
+                    <Button timer="5" startTimer={this.startTimer} />
+                    <Button timer="10" startTimer={this.startTimer} />
+                    <Button timer="15" startTimer={this.startTimer} />
+                </div>
+                <Timer timerLeft={this.state.timerLeft}/>
             </div>
         )
     }
